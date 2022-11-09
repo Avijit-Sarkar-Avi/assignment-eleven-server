d1b63a96b9e0bd30c63635bebd28df22abe57de2
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-11-09 23:38:28 +0600

    get review data in console

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('assignmentEleven').collection('services');
+        const orderCollection = client.db('assignmentEleven').collection('orders');
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -30,6 +31,12 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        });
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         })
     }
     finally {
